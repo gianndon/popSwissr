f5ecfb3fc477be88c15ddcbeca07d9dcb1ffdb6d
gianndon <gianndon@students.zhaw.ch>
2023-03-08 20:26:03 +0100

    new function

---
+++ R/f.R
@@ -0,0 +1,13 @@
+f <- function(x){
+  
+  # x-vals
+  x_val <- seq(0, x, by=0.01)
+  
+  # Function
+  shift <- sample(x=1:5, size=2, replace=TRUE)
+  f_val <- sin(shift[1]*x_val) + shift[2]
+  
+  # Plot
+  plot(x=x_val, y=f_val, type="l", xlab="x-Werte", ylab="y-Werte",
+       main=paste("Sin(",shift[1],"*x)+",shift[2]))
+}
