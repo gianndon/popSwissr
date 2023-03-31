e0c4e69d89228f22bb62e63d3b0ad475ed8bad32
gianndon <gianndon@students.zhaw.ch>
2023-03-31 17:12:40 +0200

    update get_data function

--- R/get_data.R
+++ R/get_data.R
@@ -1,12 +1,12 @@
 get_data <- function(symbols, starting_point=Sys.Date()-10*365, end_point=Sys.Date()){
-  library(quantmod)
+  # get data from yahoo finance
   dat <- do.call(cbind, sapply(X=symbols, FUN=function(x){
-    getSymbols(Symbols=x,
-               from=starting_point,
-               to=end_point,
-               auto.assign=FALSE)[, 6]
+    quantmod::getSymbols(Symbols=x,
+                         from=starting_point,
+                         to=end_point,
+                         auto.assign=FALSE)[, 6]
   })) %>%
     `colnames<-`(symbols)
-  dat_bereinigt <- na.omit(dat)
-  return(dat_bereinigt)
+  # drop na's and return data
+  return(na.omit(dat))
 }
