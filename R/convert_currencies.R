d5a16f38de4a79e6deac20f38c4d3d9f70196495
gianndon <gianndon@students.zhaw.ch>
2023-04-04 11:47:03 +0200

    update convert currencies

--- R/convert_currencies.R
+++ R/convert_currencies.R
@@ -1,7 +1,7 @@
-convert_currencies <- function(symbols){
-  data <- get_data(symbols=symbols)
-  data$GSPC.Adjusted <- data$USDCHF.X.Adjusted * data$GSPC.Adjusted
-  data$BTC.USD.Adjusted <- data$USDCHF.X.Adjusted * data$BTC.USD.Adjusted
-  data$TNX.Adjusted <- data$USDCHF.X.Adjusted * data$TNX.Adjusted
-  return(data)
+convert_currencies <- function(symbols, starting_point=Sys.Date() - 10*365, endpoint=Sys.Date()){**
+    data <- get_data(symbols=symbols, starting_point=starting_point, endpoint=endpoint)
+    data$GSPC.Adjusted <- data$USDCHF.X.Adjusted * data$GSPC.Adjusted
+    data$BTC.USD.Adjusted <- data$USDCHF.X.Adjusted * data$BTC.USD.Adjusted
+    data$TNX.Adjusted <- data$USDCHF.X.Adjusted * data$TNX.Adjusted
+    return(data)
 }
