19dadd64241c304be4a43edcbf9e00f17a5dc3d5
gianndon <gianndon@students.zhaw.ch>
2023-04-06 15:13:14 +0200

    update convert currencies function surpress warnings

--- R/convert_currencies.R
+++ R/convert_currencies.R
@@ -1,7 +1,12 @@
 convert_currencies <- function(symbols, starting_point=(Sys.Date()-30*365), end_point=Sys.Date()){
-    data <- get_data(symbols=symbols, starting_point=starting_point, end_point=end_point)
-    data$GSPC.Adjusted <- data$USDCHF.X.Adjusted * data$GSPC.Adjusted
-    data$BTC.USD.Adjusted <- data$USDCHF.X.Adjusted * data$BTC.USD.Adjusted
-    data$TNX.Adjusted <- data$USDCHF.X.Adjusted * data$TNX.Adjusted
-    return(data)
+  # supress warnings (start block)
+  defaultW <- getOption("warn"); options(warn = -1)
+  # convert currencies
+  data <- get_data(symbols=symbols, starting_point=starting_point, end_point=end_point)
+  data$GSPC.Adjusted <- data$USDCHF.X.Adjusted * data$GSPC.Adjusted
+  data$BTC.USD.Adjusted <- data$USDCHF.X.Adjusted * data$BTC.USD.Adjusted
+  data$TNX.Adjusted <- data$USDCHF.X.Adjusted * data$TNX.Adjusted
+  return(data)
+  # surpress warnings end block
+  options(warn = defaultW)
 }
