0750fe8efa343071071b46a2847e4697cacd3aaa
gianndon <gianndon@students.zhaw.ch>
2023-04-06 13:11:18 +0200

    update convert currencies function

--- R/convert_currencies.R
+++ R/convert_currencies.R
@@ -1,4 +1,4 @@
-convert_currencies <- function(symbols, starting_point=Sys.Date() - 10*365, endpoint=Sys.Date()){
+convert_currencies <- function(symbols, starting_point=Sys.Date() - 30*365, endpoint=Sys.Date()){
     data <- get_data(symbols=symbols, starting_point=starting_point, endpoint=endpoint)
     data$GSPC.Adjusted <- data$USDCHF.X.Adjusted * data$GSPC.Adjusted
     data$BTC.USD.Adjusted <- data$USDCHF.X.Adjusted * data$BTC.USD.Adjusted
