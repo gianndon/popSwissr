73b908b31beb885d404c4e277a58c5fac7b7a1dd
gianndon <gianndon@students.zhaw.ch>
2023-04-13 19:03:30 +0200

    update get rf function with SNB and ym source

---
+++ R/get_rf_SNB.R
@@ -0,0 +1,8 @@
+get_rf_SNB <- function(){
+  URL <- "https://www.snb.ch/de/iabout/stat/statrep/id/current_interest_exchange_rates#t2"
+  pagecode <- xml2::read_html(x=URL)
+  pagecode <- rvest::html_nodes(pagecode, css=".col-md-6:nth-child(3) .heading+ span")
+  pagecode <- rvest::html_text(pagecode)
+  rf <- as.numeric(substr(x=pagecode[1], start=1, stop=nchar(pagecode[1])-1))
+  return(rf)
+}
--- R/get_rf_ym.R
+++ R/get_rf_ym.R
@@ -1,4 +1,4 @@
-get_rf <- function(){
+get_rf_ym <- function(){
   # URL <- "https://www.snb.ch/de/iabout/stat/statrep/id/current_interest_exchange_rates#t2"  # SNB URL
   URL <- "https://www.yourmoney.ch/ym/details/4961368%2C1526%2C1#Tab0"  # yourmoney URL
   pagecode <- xml2::read_html(x=URL)
@@ -7,6 +7,6 @@ get_rf <- function(){
   pagecode <- rvest::html_text(pagecode)
   # rf <- as.numeric(substr(x=pagecode[1], start=1, stop=nchar(pagecode[1])-1))  # rf return SNB
   pagecode_clean <- gsub(" ", "", pagecode, fixed = TRUE)
-  rf <- as.numeric(substr(x=pagecode_clean[1], start=1, stop=nchar(pagecode_clean[1])-1))
+  rf <- as.numeric(substr(x=pagecode_clean[1], start=1, stop=nchar(pagecode_clean[1])-1))  # rf return yourmoney
   return(rf)
 }
