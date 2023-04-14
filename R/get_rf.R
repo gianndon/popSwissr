8a500f790659b1be992433edce141576e7a5f304
gianndon <gianndon@students.zhaw.ch>
2023-04-14 10:44:01 +0200

    update package

--- R/get_rf.R
+++ R/get_rf.R
@@ -1,11 +1,8 @@
 get_rf_ym <- function(){
-  # URL <- "https://www.snb.ch/de/iabout/stat/statrep/id/current_interest_exchange_rates#t2"  # SNB URL
-  URL <- "https://www.yourmoney.ch/ym/details/4961368%2C1526%2C1#Tab0"  # yourmoney URL
+  URL <- "https://www.yourmoney.ch/ym/details/4961368%2C1526%2C1#Tab0"
   pagecode <- xml2::read_html(x=URL)
-  pagecode <- rvest::html_nodes(pagecode, css=".detailPrice")  # yourmoney SARON
-  # pagecode <- rvest::html_nodes(pagecode, css=".col-md-6:nth-child(3) .heading+ span")  # SNB SARON
+  pagecode <- rvest::html_nodes(pagecode, css=".detailPrice")
   pagecode <- rvest::html_text(pagecode)
-  # rf <- as.numeric(substr(x=pagecode[1], start=1, stop=nchar(pagecode[1])-1))  # rf return SNB
   pagecode_clean <- gsub(" ", "", pagecode, fixed = TRUE)
   rf <- as.numeric(substr(x=pagecode_clean[1], start=1, stop=nchar(pagecode_clean[1])-1))  # rf return yourmoney
   return(rf)
