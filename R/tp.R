8be9660a206e1fda9c580a95645aa176a9bef7a8
gianndon <gianndon@students.zhaw.ch>
2023-05-05 17:11:05 +0200

    t

--- R/tp.R
+++ R/tp.R
@@ -5,10 +5,10 @@ tp <- function(assets, rf=0.01, p_year=260){
   Sigma <- cov(assets); Sigma_inv <- solve(Sigma)
   # compute weights
   weights <- Sigma_inv %*% (yearly_return - rf*rep(1, dim(Sigma)[1]))
-  weights_scal <- weights/sum(weights); weights_scal <- as.vector(weights_scal); names(weights_scal) <- colnames(assets)
+  weights_scal <- weights/sum(weights); weights_scal <- as.vector(weights_scal) # ; names(weights_scal) <- colnames(assets)
   # compute return and volatility
-  return_TP <- t(weights_scal) %*% yearly_return; return_TP <- as.vector(return_TP); names(return_TP) <- "Return Portfolio"
-  volatility_TP <- sqrt(t(weights_scal) %*% Sigma %*% weights_scal)*sqrt(260); volatility_TP <- as.vector(volatility_TP); names(volatility_TP) <- "Volatility Portfolio"
+  return_TP <- t(weights_scal) %*% yearly_return; return_TP <- as.vector(return_TP) #; names(return_TP) <- "Return Portfolio"
+  volatility_TP <- sqrt(t(weights_scal) %*% Sigma %*% weights_scal)*sqrt(260); volatility_TP <- as.vector(volatility_TP) #; names(volatility_TP) <- "Volatility Portfolio"
   # return
   return(c(weights_scal, abs(return_TP), volatility_TP))
 }
